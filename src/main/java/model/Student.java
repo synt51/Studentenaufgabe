6ba08ae1cb2ac3020314a93cf11654f45d74dd60
Heinz Schloemer <heinz.schloemer@gmx.net>
2021-12-06 15:23:10 +0100

    override equals & hashCode in Student class

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Student {
 
     int age;
@@ -68,5 +70,18 @@ public class Student {
                 ", Nachname='" + lastName + '\'' +
                 ", Geschlecht='" + gender + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(gender, student.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age, firstName, lastName, gender);
     }
 }
