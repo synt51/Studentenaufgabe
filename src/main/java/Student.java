08e258dbd72097436dc7abdbfc761a11a6a93614
Heinz Schloemer <heinz.schloemer@gmx.net>
2021-12-06 13:18:48 +0100

    fixed

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,67 @@
 public class Student {
+
+    private int age;
+    private String firstName;
+    private String lastName;
+    private String gender;
+
+    public Student(String lastName, String firstName, String gender, int age){
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.gender = gender;
+        this.age = age;
+    }
+
+    int getAge(){
+        return age;
+    }
+    void setAge(int age){
+        this.age = age;
+    }
+
+    String getFirstName(){
+        return firstName;
+    }
+    boolean setFirstName(String firstName){
+        if(firstName == null || firstName.isEmpty()){
+            return false;
+        }
+        this.firstName = firstName;
+        return true;
+    }
+
+    String getLastName(){
+        return lastName;
+    }
+    boolean setLastName(String lastName){
+        if(lastName == null || lastName.isEmpty()){
+            return false;
+        }
+        this.lastName = lastName;
+        return true;
+    }
+
+    String getGender(){
+        return gender;
+    }
+    boolean setGender(String gender){
+        if(gender == "male" || gender == "female" || gender == "diverse") {
+            this.gender = gender;
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Student andreas = new Student("Müller","Andreas", "male", 25);
+        String andreasLastName = andreas.getLastName();
+        String andreasFirstName = andreas.getFirstName();
+        String andreasGender = andreas.getGender();
+        int andreasAge = andreas.getAge();
+
+        System.out.println("Nachname: " + andreasLastName);
+        System.out.println("Vorname: " + andreasFirstName);
+        System.out.println("Geschlecht: " + andreasGender);
+        System.out.println("Alter: " + andreasAge);
+    }
 }
---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,63 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    Student student = new Student("Müller", "Andreas", "male", 25);
+
+    @Test
+    void checkLastName(){
+        //given
+        String lastName = "Müller";
+        //when
+        assertTrue(student.setLastName(lastName));
+        //then
+        assertEquals(lastName, student.getLastName());
+    }
+    @Test
+    void checkLastNameFalse(){
+        String lastName = "Müller";
+        assertFalse(student.setLastName(null));
+        assertNotEquals("Schmidt", student.getFirstName());
+    }
+    @Test
+    void checkFirstName(){
+        String firstName = "Andreas";
+        assertTrue(student.setFirstName(firstName));
+        assertEquals(firstName, student.getFirstName());
+    }
+    @Test
+    void checkFirstNameFalse(){
+        String firstName = "Andreas";
+        assertFalse(student.setFirstName(null));
+        assertNotEquals("Klaus", student.getFirstName());
+    }
+
+    @Test
+    void checkGender(){
+        String gender = "male";
+        assertTrue(student.setGender(gender));
+        assertEquals(gender, student.getGender());
+    }
+    @Test
+    void checkGenderFalse(){
+        String gender = "male";
+        assertFalse(student.setGender(null));
+        assertNotEquals("ajdaoid", student.getGender());
+    }
+
+    @Test
+    void checkAge(){
+        int age = 25;
+        student.setAge(age);
+        assertEquals(age, student.getAge());
+    }
+    @Test
+    void checkAgeFalse(){
+        int age = 25;
+        student.setAge(age);
+        assertNotEquals(20, student.getAge());
+    }
+
+}
