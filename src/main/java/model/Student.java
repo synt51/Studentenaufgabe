3ae0936c84bdbed2ca8bf9eeba559240bfe0dd65
Heinz Schloemer <heinz.schloemer@gmx.net>
2021-12-07 13:59:41 +0100

    Interface done

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -33,28 +33,10 @@ public interface Student {
 
     @Override
     String toString();
-//    {
-//        return "Student{" +
-//                "Nachname= '" + lastName + '\'' +
-//                ", Vorname= '" + firstName + '\'' +
-//                ", ID= " + id +
-//                ", Geschlecht= '" + gender + '\'' +
-//                ", Alter= " + age +
-//                '}';
-//    }
 
     @Override
     boolean equals(Object o);
-//    {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Student student = (Student) o;
-//        return age == student.age && id == student.id && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(gender, student.gender);
-//    }
 
     @Override
     int hashCode();
-//    {
-//        return Objects.hash(age, firstName, lastName, gender, id);
-//    }
 }
